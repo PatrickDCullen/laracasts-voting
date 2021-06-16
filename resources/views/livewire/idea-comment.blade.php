3c4bc8d6c4db6b2d430e7df475a81984e6ec8451
PatrickDCullen <cullenpatrickd@gmail.com>
2021-06-16 11:33:56 -0400

    End Ep 49: Add Comments

--- resources/views/livewire/idea-comment.blade.php
+++ resources/views/livewire/idea-comment.blade.php
@@ -12,7 +12,7 @@
             </a>
         </div>
         <div class="w-full md:mx-4">
-            <div class="text-gray-600 line-clamp-3">
+            <div class="text-gray-600">
                 {{ $comment->body }}
             </div>
 
@@ -20,6 +20,10 @@
                 <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                     <div class="font-bold text-gray-900">{{ $comment->user->name }}</div>
                     <div>&bull;</div>
+                    @if ($comment->user->id === $ideaUserId)
+                        <div class="rounded-full border bg-gray-100 px-3 py-1">OP</div>
+                        <div>&bull;</div>
+                    @endif
                     <div>{{ $comment->created_at->diffForHumans() }}</div>
                 </div>
                 <div
