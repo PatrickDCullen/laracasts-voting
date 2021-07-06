cdb25218e16f72de3a4ee7fcf930b4bfd01cab68
PatrickDCullen <cullenpatrickd@gmail.com>
2021-07-06 06:07:14 -0400

    End Ep 55: Edit and Delete Comments Test

---
+++ tests/Feature/Comments/DeleteCommentTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Tests\Feature\Comments;
+
+use Tests\TestCase;
+use App\Models\Idea;
+use App\Models\User;
+use Livewire\Livewire;
+use App\Models\Comment;
+use App\Models\Category;
+use Illuminate\Http\Response;
+use App\Http\Livewire\EditIdea;
+use App\Http\Livewire\IdeaShow;
+use App\Http\Livewire\EditComment;
+use App\Http\Livewire\IdeaComment;
+use App\Http\Livewire\DeleteComment;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DeleteCommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function shows_delete_comment_livewire_component_when_user_has_authorization()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $this->actingAs($user)
+            ->get(route('idea.show', $idea))
+            ->assertSeeLivewire('delete-comment');
+    }
+
+    /** @test */
+    public function does_not_show_delete_comment_livewire_component_when_user_does_not_have_authorization()
+    {
+        $idea = Idea::factory()->create();
+
+        $this->get(route('idea.show', $idea))
+            ->assertDontSeeLivewire('delete-comment');
+    }
+
+    /** @test */
+    public function delete_comment_is_set_correctly_when_user_clicks_it_from_menu()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $comment = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(DeleteComment::class)
+            ->call('setDeleteComment', $comment->id)
+            ->assertEmitted('deleteCommentWasSet');
+    }
+
+    /** @test */
+    public function deleting_a_comment_works_when_user_has_authorization()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $comment = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        $this->assertEquals(1, Comment::count());
+
+        Livewire::actingAs($user)
+            ->test(DeleteComment::class)
+            ->call('setDeleteComment', $comment->id)
+            ->call('deleteComment')
+            ->assertEmitted('commentWasDeleted');
+
+        $this->assertDatabaseMissing('comments', [
+            'idea_id' => $idea->id,
+            'user_id' => $user->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        $this->assertEquals(0, Comment::count());
+    }
+
+    /** @test */
+    public function deleting_a_comment_does_not_work_when_user_does_not_have_authorization()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $comment = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(DeleteComment::class)
+            ->call('setDeleteComment', $comment->id)
+            ->call('deleteComment')
+            ->assertNotEmitted('commentWasDeleted')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+
+        $this->assertDatabaseHas('comments', [
+            'idea_id' => $idea->id,
+            'body' => 'This is my first comment'
+        ]);
+    }
+
+    /** @test */
+    public function deleting_a_comment_shows_on_menu_when_user_has_authorization()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $comment = Comment::factory()->create([
+            'user_id' => $user->id,
+            'idea_id' => $idea->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(IdeaComment::class, [
+                'comment' => $comment,
+                'ideaUserId' => $idea->user_id,
+            ])
+            ->assertSee('Delete Comment');
+    }
+
+    /** @test */
+    public function deleting_a_comment_does_not_show_on_menu_when_user_does_not_have_authorization()
+    {
+        $user = User::factory()->create();
+        $idea = Idea::factory()->create();
+
+        $comment = Comment::factory()->create([
+            'idea_id' => $idea->id,
+            'body' => 'This is my first comment'
+        ]);
+
+        Livewire::actingAs($user)
+            ->test(IdeaComment::class, [
+                'comment' => $comment,
+                'ideaUserId' => $idea->user_id,
+            ])
+            ->assertDontSee('Delete Comment');
+    }
+}
